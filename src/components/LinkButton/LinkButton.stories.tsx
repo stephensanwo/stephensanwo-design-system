ad0496a83b5be53a518d4a6feb3daff24d90f619
stephen sanwo <Stephen.sanwo@icloud.com>
2022-12-23 17:47:04 +0100

    base design system init

---
+++ src/components/LinkButton/LinkButton.stories.tsx
@@ -0,0 +1,37 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import LinkButton from "./LinkButton";
+import { action } from "@storybook/addon-actions";
+
+export default {
+  title: "Components/LinkButton",
+  component: LinkButton,
+  argTypes: {},
+} as ComponentMeta<typeof LinkButton>;
+
+const Template: ComponentStory<typeof LinkButton> = (args) => (
+  <LinkButton {...args}></LinkButton>
+);
+
+export const Primary = Template.bind({});
+Primary.args = {
+  kind: "primary",
+  text: "Collaborate with me",
+  href: "https://www.stephensanwo.dev",
+  hasIcon: true,
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  kind: "secondary",
+  text: "Collaborate with me",
+  href: "https://www.stephensanwo.dev",
+  hasIcon: true,
+};
+
+export const Danger = Template.bind({});
+Danger.args = {
+  kind: "danger",
+  text: "Visit unknown url",
+  href: "https://www.stephensanwo.dev",
+  hasIcon: true,
+};
